65d3ce6ed699c9108011ebfd9e1a8a6108005354
Wu, Ruth <ruth.wu@accenture.com>
2018-02-13 18:28:52 -0800

    arrays problem 2

--- arrays.js
+++ arrays.js
@@ -11,3 +11,27 @@ function isUnique(str) {
   }
   return true;
 }
+
+//2 Check Permutation
+//Assume white space and case sensitivity are important
+
+function checkPermutation(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  var sortedStr1 = this.sortString(str1);
+  var sortedStr2 = this.sortString(str2);
+
+  for (i = 0; i < sortedStr1.length; i++) {
+    if (sortedStr1[i] !== sortedStr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sortString(str) {
+  return str.split('').sort().join('');
+}
+
